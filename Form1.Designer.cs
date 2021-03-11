abd0a6950867591d470890c0c1c66d01bc731ddb
bxb <behruz.numonov@yandex.ru>
2021-03-11 09:02:44 +0500

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace TrafficAnalyzer
+{
+    partial class SnifferForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.cmbInterfaces = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(12, 12);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(656, 349);
+            this.treeView1.TabIndex = 0;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(12, 375);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(167, 54);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // cmbInterfaces
+            // 
+            this.cmbInterfaces.FormattingEnabled = true;
+            this.cmbInterfaces.Location = new System.Drawing.Point(206, 393);
+            this.cmbInterfaces.Name = "cmbInterfaces";
+            this.cmbInterfaces.Size = new System.Drawing.Size(462, 21);
+            this.cmbInterfaces.TabIndex = 2;
+            // 
+            // SnifferForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(680, 441);
+            this.Controls.Add(this.cmbInterfaces);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.treeView1);
+            this.Name = "SnifferForm";
+            this.Text = "Traffic Analyzer (Sniffer) ";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.SnifferForm_FormClosing);
+            this.Load += new System.EventHandler(this.SnifferForm_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.ComboBox cmbInterfaces;
+    }
+}
+
